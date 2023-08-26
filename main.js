f5240fb39e5ff3c5011be299a48fbd9b4319f333
JadenKeller <jadkeller@csumb.edu>
2023-08-26 12:55:38 -0700

    change

--- main.js
+++ main.js
@@ -1,31 +1,35 @@
-import * as three from "/node_modules/three/build/three.module.js";
+// import * as three from "/node_modules/three/build/three.module.js";
 
-window.addEventListener('resize', onWindowResize, false);
 
-const scene = new three.Scene();
-const camera = new three.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-const renderer = new three.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
 
-const geometry = new three.BoxGeometry(1, 1, 1, 1);
-const material = new three.MeshBasicMaterial({color:0x00ff00});
+// window.addEventListener('resize', onWindowResize, false);
 
-const cube = new three.Mesh(geometry, material);
-scene.add(cube);
+// const scene = new three.Scene();
+// const camera = new three.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+// const renderer = new three.WebGLRenderer();
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// document.body.appendChild(renderer.domElement);
 
-camera.position.z = 5;
+// const geometry = new three.BoxGeometry(1, 1, 1, 1);
+// const material = new three.MeshBasicMaterial({color:0x00ff00});
 
-function onWindowResize(){
-    camera.aspect = window.innerWidth/window.innerHeight;
-    camera.updateProjectionMatrix();
+// const cube = new three.Mesh(geometry, material);
+// scene.add(cube);
 
-    renderer.setSize(window.innerWidth, window.innerHeight);
-}
+// camera.position.z = 5;
 
-animate();
-function animate(){
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-    cube.rotation.x += 0.01;
-}
+// function onWindowResize(){
+//     camera.aspect = window.innerWidth/window.innerHeight;
+//     camera.updateProjectionMatrix();
+
+//     renderer.setSize(window.innerWidth, window.innerHeight);
+// }
+
+// animate();
+// function animate(){
+//     requestAnimationFrame(animate);
+//     renderer.render(scene, camera);
+//     cube.rotation.x += 0.01;
+// }
+
+console.log("happen");
