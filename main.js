b11ce5027e486b329beaad4588fb45b815b567cf
JadenKeller <jadkeller@csumb.edu>
2023-08-26 15:23:58 -0700

    thing2

--- main.js
+++ main.js
@@ -3,33 +3,32 @@ console.log("happen");
 import * as three from "/node_modules/three/build/three.module.js";
 
 
-// window.addEventListener('resize', onWindowResize, false);
+window.addEventListener('resize', onWindowResize, false);
 
-// const scene = new three.Scene();
-// const camera = new three.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-// const renderer = new three.WebGLRenderer();
-// renderer.setSize(window.innerWidth, window.innerHeight);
-// document.body.appendChild(renderer.domElement);
+const scene = new three.Scene();
+const camera = new three.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+const renderer = new three.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-// const geometry = new three.BoxGeometry(1, 1, 1, 1);
-// const material = new three.MeshBasicMaterial({color:0x00ff00});
+const geometry = new three.BoxGeometry(1, 1, 1, 1);
+const material = new three.MeshBasicMaterial({color:0x00ff00});
 
-// const cube = new three.Mesh(geometry, material);
-// scene.add(cube);
+const cube = new three.Mesh(geometry, material);
+scene.add(cube);
 
-// camera.position.z = 5;
+camera.position.z = 5;
 
-// function onWindowResize(){
-//     camera.aspect = window.innerWidth/window.innerHeight;
-//     camera.updateProjectionMatrix();
+function onWindowResize(){
+    camera.aspect = window.innerWidth/window.innerHeight;
+    camera.updateProjectionMatrix();
 
-//     renderer.setSize(window.innerWidth, window.innerHeight);
-// }
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
-// animate();
-// function animate(){
-//     requestAnimationFrame(animate);
-//     renderer.render(scene, camera);
-//     cube.rotation.x += 0.01;
-// }
-
+animate();
+function animate(){
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    cube.rotation.x += 0.01;
+}
